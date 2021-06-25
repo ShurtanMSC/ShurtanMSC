7aa82cbe8ae5862d826db16643af77adb53809a8
lordu98 <u.makhmudov211326@gmail.com>
2021-06-25 09:41:50 +0500

    api/admin controller

--- src/main/java/uz/neft/controller/SuperAdminController.java
+++ src/main/java/uz/neft/controller/SuperAdminController.java
@@ -1,13 +1,59 @@
 package uz.neft.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.neft.dto.UserDto;
+import uz.neft.payload.ApiResponse;
+import uz.neft.repository.UserRepository;
+import uz.neft.service.UserService;
+import uz.neft.utils.Converter;
 
 @RestController
 @RequestMapping("api/admin/")
 public class SuperAdminController {
 
-    /** User qo'shish **/
+    private final UserService userService;
+
+    @Autowired
+    public SuperAdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * User qo'shish
+     **/
+
+    @PostMapping("/save")
+    public HttpEntity<?> save(@RequestBody UserDto dto) {
+        ApiResponse save = userService.save(dto);
+        return ResponseEntity.ok(save);
+    }
+
+    @PutMapping("/edit")
+    public HttpEntity<?> edit(@RequestBody UserDto dto) {
+        ApiResponse edit = userService.edit(dto);
+        return ResponseEntity.ok(edit);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public HttpEntity<?> delete(@PathVariable Integer id) {
+        ApiResponse delete = userService.delete(id);
+        return ResponseEntity.ok(delete);
+    }
+
+    @GetMapping("/users")
+    public HttpEntity<?> all() {
+        ApiResponse all = userService.findAll();
+        return ResponseEntity.ok(all);
+    }
+
+    @GetMapping("/user/{id}")
+    public HttpEntity<?> userById(@PathVariable Integer id) {
+        ApiResponse byId = userService.findById(id);
+        return ResponseEntity.ok(byId);
+    }
 
 
 }
--- src/main/java/uz/neft/service/UserService.java
+++ src/main/java/uz/neft/service/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import uz.neft.dto.UserDto;
+import uz.neft.entity.Role;
 import uz.neft.entity.User;
 import uz.neft.payload.ApiResponse;
 import uz.neft.repository.RoleRepository;
@@ -11,36 +12,103 @@ import uz.neft.repository.UserRepository;
 import uz.neft.utils.Converter;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private Converter converter;
+
+    private final UserRepository userRepository;
+    private final Converter converter;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    private RoleRepository roleRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    public UserService(UserRepository userRepository, Converter converter, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.converter = converter;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public ApiResponse save(UserDto dto) {
+        try {
+            if (dto.getId() != null) return converter.apiError();
+            if (!roleRepository.existsById(dto.getRoleId())) return converter.apiError();
+            User user = User
+                    .builder()
+                    .username(dto.getUsername())
+                    .phone(dto.getPhone())
+                    .fio(dto.getFio())
+                    .active(dto.isActive())
+                    .email(dto.getEmail())
+                    .password(passwordEncoder.encode(dto.getPassword()))
+                    .roles(Collections.singleton(roleRepository.findById(dto.getRoleId()).get()))
+                    .build();
+            user = userRepository.save(user);
+            return converter.apiSuccess("Saved", user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return converter.apiError();
+        }
+    }
 
-        if (dto.getId() != null) return converter.apiError();
-        if (!roleRepository.existsById(dto.getRoleId())) return converter.apiError();
-        User user = User
-                .builder()
-                .username(dto.getUsername())
-                .phone(dto.getPhone())
-                .fio(dto.getFio())
-                .active(dto.isActive())
-                .email(dto.getEmail())
-                .password(passwordEncoder.encode(dto.getPassword()))
-                .roles(Collections.singleton(roleRepository.findById(dto.getRoleId()).get()))
-                .build();
-        user = userRepository.save(user);
-        return converter.apiSuccess("Saved", user);
+    public ApiResponse edit(UserDto dto) {
+        try {
+            User editingUser = new User();
+            Optional<User> byId = userRepository.findById(dto.getId());
+            if (byId.isPresent()) {
+                editingUser = byId.get();
+            }
+            editingUser.setActive(dto.isActive());
+            editingUser.setEmail(dto.getEmail());
+            editingUser.setUsername(dto.getUsername());
+            editingUser.setFio(dto.getFio());
+
+            if (!passwordEncoder.matches(editingUser.getPassword(), dto.getPassword())) {
+            editingUser.setPassword(passwordEncoder.encode(dto.getPassword()));
+            }
+
+            editingUser.setPhone(dto.getPhone());
+            Optional<Role> byId1 = roleRepository.findById(dto.getRoleId());
+            editingUser.setRoles(Collections.singleton(byId1.get()));
+            User editedUser = userRepository.save(editingUser);
+            return converter.apiSuccess("Edited", editedUser);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return converter.apiError();
+        }
+    }
+
+    public ApiResponse findById(Integer id) {
+        try {
+            if (id != null) {
+                Optional<User> byId = userRepository.findById(id);
+                if (byId.isPresent()) {
+                    Optional<User> byId1 = userRepository.findById(id);
+                    return converter.apiSuccess(byId1.get());
+                } else {
+                    return converter.apiError("User not found");
+                }
+            }
+            return converter.apiError("Id null");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return converter.apiError("Error in finding user", e);
+        }
+    }
+
+    public ApiResponse findAll() {
+        try {
+            List<User> all = userRepository.findAll();
+            List<UserDto> collect = all.stream().map(item -> converter.userToUserDto(item)).collect(Collectors.toList());
+
+            return converter.apiSuccess(collect);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return converter.apiError("Error in finding user", e);
+        }
     }
 
     public ApiResponse delete(Integer id) {
