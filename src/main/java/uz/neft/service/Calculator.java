69d23d7653a27197484f5062ac6e8669671dc211
kali <primat.2000@mail.ru>
2021-07-26 14:33:54 +0500

    Bug fixes

--- src/main/java/uz/neft/service/Calculator.java
+++ src/main/java/uz/neft/service/Calculator.java
@@ -1,22 +1,28 @@
 package uz.neft.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uz.neft.repository.MiningSystemRepository;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class Calculator {
 
-    public static double generalCalculate(double R_u, double T_u) {
-        double C;
-        double delta;
-        double p_otn;
-        double Z;
-        double RO = 0;
-        double T_pr;
-        double P_pr;
 
-
-
-    }
+//    public static double generalCalculate(double P_u, double T_u) {
+//        double C;
+//        double delta;
+//        double p_otn;
+//        double Z;
+//        double RO = 0;
+//        double T_pr;
+//        double P_pr;
+//
+//
+//
+//    }
 
     // Средний расчетный дебит скважин месторождения Шуртан (D СКВ), 1×103 m3,
     // определяется по формуле
@@ -26,24 +32,20 @@ public class Calculator {
 
     public static double averageProductionRate(
             double C,
-            double R_u,
+            double P_u,
             double p_otn,
             double Z,
             double T_u
     ) {
 
-        double delta=correctionFactor();
+//        double delta=correctionFactor();
+        double delta=0;
         double D = 0;
-        D = (C * R_u * delta) / (0.1013 * Math.sqrt(p_otn * T_u * Z));
+        D = (C * P_u * delta) / (0.1013 * Math.sqrt(p_otn * T_u * Z));
         return D;
     }
 
-    // ρ отн – относительная плотность газа (gazning nisbiy zichligi);
-    public static double relativeDensity(double roGas, double roWeather) {
-        double RO = 0;
-        RO = roGas / roWeather;
-        return RO;
-    }
+
 
     //  Поправочный коэффициент, зависящий от приведенных давления и температуры (δ),
     //  определяется графически
@@ -70,9 +72,36 @@ public class Calculator {
     // yoki quyidagi formula bo'yicha:
 
     //Z = 1 – [(Ру – 6) · (0,345·10-2 · ∆в – 0,446·10-3) + 0,015] · [1,3 – 0,0144 · (Ту – 283,2)]
-    public static double superCompressFactor(double R_u, double T_u, double p_otn) {
-        double Z = 1 - ((R_u - 6) * (0.345 * 0.01 * p_otn - 0.446 * 0.001) + 0.015) * (1.3 - 0.0144 * (T_u - 283.2));
+    public static double superCompressFactor(double P_u, double T_u, double p_otn) {
+        double Z = 1 - ((P_u - 6) * (0.345 * 0.01 * p_otn - 0.446 * 0.001) + 0.015) * (1.3 - 0.0144 * (T_u - 283.2));
         return Z;
+    }
+
+
+
+
+
+
+    // ρ отн – относительная плотность газа (gazning nisbiy zichligi);
+    public static double relativeDensity(double roGas, double roAir) {
+        double RO = 0;
+        RO = roGas / roAir;
+        return RO;
+    }
+
+    public static double pseudoCriticalPressure(List<Double> molarFractions,
+                                                List<Double> criticalPressure
+    ){
+        if (molarFractions.size()!=criticalPressure.size()) return 0;
+//        if (molarFractions.stream().mapToDouble(n->n).sum()!=100) return 0;
+
+        double s=0;
+
+        for (int i = 0; i < molarFractions.size() ; i++) {
+            s+=molarFractions.get(i)*criticalPressure.get(i);
+        }
+//        System.out.println(s);
+        return s;
     }
 
 
