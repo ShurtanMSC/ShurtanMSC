f625f5ac4a00bc3c5bb2c21688d1e7d186d2752f
lordu98 <u.makhmudov211326@gmail.com>
2021-07-28 22:07:17 +0500

    MiningSystemGasComposition Service methods for molar fractions FULL DONE

--- src/main/java/uz/neft/service/GasCompositionService.java
+++ src/main/java/uz/neft/service/GasCompositionService.java
@@ -1,5 +1,5 @@
 package uz.neft.service;
-
+//lord
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uz.neft.dto.CollectionPointDto;
