97d24baf05450ea28f9031674b0916a368464df3
mahmudsalomov <mahmud.salomov.salomov@gmail.com>
2021-08-26 16:04:33 +0500

    Modified

--- src/main/java/uz/neft/controller/AuthController.java
+++ src/main/java/uz/neft/controller/AuthController.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import uz.neft.entity.User;
 import uz.neft.payload.ResToken;
 import uz.neft.payload.SignIn;
+import uz.neft.secret.CurrentUser;
 import uz.neft.service.AuthService;
 import uz.neft.utils.Converter;
 
@@ -33,5 +35,11 @@ public class AuthController {
         return ResponseEntity.ok(converter.apiSuccess());
     }
 
+    @GetMapping("/me")
+    public HttpEntity<?> me(@CurrentUser User user){
+        if (user!=null){
+            return converter.apiSuccess200(converter.userToUserDto(user));
+        }else return null;
+    }
 
 }
