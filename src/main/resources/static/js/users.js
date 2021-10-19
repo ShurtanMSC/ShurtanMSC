3ec80984fd0a43096b9680dd0ae1f5615154e2d5
Lord <u.makhmudov211326@gmail.com>
2021-10-19 11:11:30 +0500

    user done

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -1,7 +1,11 @@
+let usersList = []
+
 function getAllUsers() {
     axios.get("/api/admin/user/all")
         .then(function (response) {
-            console.log(response.data)
+            if (response.data.message === "OK") {
+                usersList = response.data.object
+            }
             document.getElementById("userTable").innerHTML = createViewTable(response.data.object)
         })
         .catch(function (error) {
@@ -12,13 +16,21 @@ function getAllUsers() {
 function getAllRoles() {
     axios.get("/api/admin/role/all")
         .then(function (response) {
-            // console.log(response.data)
+            console.log(response.data)
+            document.getElementById("inputGroupSelect03").innerHTML = addOptionRoles(response.data)
+
         })
         .catch(function (error) {
             console.log(error)
         })
 }
 
+document.getElementById('addUserBtn').addEventListener('click',addUserBtn)
+
+function addUserBtn() {
+    document.getElementById('addOrEditUserH3').innerText = 'Добавить пользователя'
+    document.getElementById('addOrEditUserBtn').innerText = 'Добавить'
+}
 function resetAndCloseForm() {
     document.getElementById('closeFormBtn').click();
     document.getElementById('addOrEditUserForm').reset();
@@ -30,7 +42,6 @@ function addOrEditUser(event) {
 
     const data = {}
     formData.forEach((value, key) => (data[key] = value));
-    console.log(data);
 
     let config = {
         method: '',
@@ -39,9 +50,11 @@ function addOrEditUser(event) {
     };
 
     if (data.id === "" || data.id == null) {
+
         config.method = 'post';
         config.url = '/api/admin/user/add'
     } else {
+
         config.method = 'put';
         config.url = '/api/admin/user/edit'
     }
@@ -56,10 +69,26 @@ function addOrEditUser(event) {
         });
 }
 
+function editUser(id) {
+    document.getElementById('addOrEditUserH3').innerText = 'Редактировать'
+    document.getElementById('addOrEditUserBtn').innerText = 'Редактировать'
+    let editUser = usersList.find(user => user.id == id)
+    let formField = document.getElementById('addOrEditUserForm')
+
+    formField['id'].value = editUser.id;
+    formField['username'].value = editUser.username;
+    formField['email'].value = editUser.email;
+    formField['password'].value = editUser.password;
+    formField['phone'].value = editUser.phone;
+    formField['fio'].value = editUser.fio;
+    formField['roleId'].value = editUser.roleId;
+
+}
+
 function deleteUser(id) {
-    axios.delete("/api/admin/user/delete/"+id)
+    axios.delete("/api/admin/user/delete/" + id)
         .then(function (response) {
-            console.log(response.data)
+            // console.log(response.data)
             getAllUsers()
         })
         .catch(function (error) {
@@ -77,9 +106,17 @@ function createViewTable(users) {
             "                                    <td>" + user.roleName + "</td>\n" +
             "                                    <td>" + user.phone + "</td>\n" +
             "                                    <td>" + user.email + "</td>\n" +
-            "                                    <td><button class='btn btn-success' id='btn-edit-user'>Редактировать</button></td>\n" +
-            "                                    <td><button class='btn btn-danger' id='btn-edit-user' value='"+user.id+"' onclick='deleteUser(this.value)'>Удалить</button></td>\n" +
+            "                                    <td><button data-target=\"#exampleModalCenter\" data-toggle=\"modal\" class='btn btn-success' id='btn-edit-user' value='" + user.id + "' onclick='editUser(this.value)'>Редактировать</button></td>\n" +
+            "                                    <td><button class='btn btn-danger' id='btn-edit-user' value='" + user.id + "' onclick='deleteUser(this.value)'>Удалить</button></td>\n" +
             "                                </tr>"
+    })
+    return out;
+}
+
+function addOptionRoles(roles) {
+    let out = "<option selected>Роли</option>";
+    roles.map(role => {
+        out += "<option value='"+role.id+"'>"+role.roleName+"</option>"
     })
     return out;
 }
