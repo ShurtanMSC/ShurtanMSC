a00fe7364592feaf5644e6948d7200081e9b184b
Nodirbotirov <nbotirov2000@gmail.comgit config --global user.email nbotirov2000@gmail.com>
2022-01-07 11:57:47 +0500

    aa

--- src/main/java/uz/neft/service/action/CollectionPointActionService.java
+++ src/main/java/uz/neft/service/action/CollectionPointActionService.java
@@ -372,7 +372,7 @@ public class CollectionPointActionService {
             if (miningSystem.isPresent()) {
                 List<Uppg> uppgs = uppgRepository.findAllByMiningSystem(miningSystem.get());
 
-                List<FakeUppg> fakeUppgList = fakeService.all();
+                List<FakeUppg> fakeUppgList = fakeService.allSimulator();
 
                 if (fakeUppgList.size() == 2 && uppgs.size() <= 2) {
                     UppgAction uppgAction1 = UppgAction
